7a6ce8a53ea090decdeb4728b592495c494881fc
Spudgun <jared.rj.white@gmail.com>
2024-11-30 14:15:25 +0000

    feat: further styling for the webpage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const containerDimensions = 480;
 
 function setupGrid(gridSize) {
-	const container = document.getElementById('container');
+	const container = document.getElementById('grid-container');
 	container.innterHtml = '';
 	container.style.width = containerDimensions + 'px';
 	let gridDimensions = containerDimensions / gridSize;
@@ -13,6 +13,17 @@ function setupGrid(gridSize) {
 		childElement.style.height = gridDimensions + 'px';
 		container.appendChild(childElement);
 	}
+}
+
+// Function for returning a random color.
+//Credit: https://stackoverflow.com/questions/1484506/random-color-generator
+function generateRandomColor() {
+	let maxVal = 0xffffff; // 16777215
+	let randomNumber = Math.random() * maxVal;
+	randomNumber = Math.floor(randomNumber);
+	randomNumber = randomNumber.toString(16);
+	let randColor = randomNumber.padStart(6, '0');
+	return `#${randColor.toUpperCase()}`;
 }
 
 setupGrid(64);
